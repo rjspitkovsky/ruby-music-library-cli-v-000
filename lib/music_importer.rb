89aeb6591870b63a12dda49a3d80cb5a32437ef2
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-19 19:13:21 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,31 +6,23 @@ class MusicImporter
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-  end
+      Dir.entries(path).each do |filename|
+        @@filenames << filename
+      end
+      @@filenames.reject! do |filename|
+        filename == "." || filename == ".."
+      end
+    end
+
+  # def files
+  #   @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  # end
 
   def import
     files.each{ |f| Song.create_from_filename(f) }
   end
 end
 
-
-# class MusicImporter
-#
-#
-#   attr_reader :path
-#
-#   def initialize(path)
-#     @path = path
-#   end
-#
-#   def files
-#     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-#   end
-#
-#   def import
-#     files.each{ |f| Song.create_from_filename(f) }
-#   end
 #   #
 #   # attr_accessor :path
 #   #
