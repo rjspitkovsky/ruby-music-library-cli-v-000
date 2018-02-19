41de30c673a423cce374d02f5e0ec27ae174e1e1
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-19 19:29:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,18 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist = artist if artist
-    self.genre = genre if genre
+    # self.artist = artist if artist
+    # self.genre = genre if genre
+      if artist != nil
+          self.artist=(artist)
+        else
+        @artist = artist
+      end
+      if genre != nil
+        self.genre=(genre)
+      else
+        @genre = genre
+      end
   end
 
   def artist=(artist)
@@ -65,8 +75,6 @@ class Song
 end
 
 
-# require 'pry'
-#
 # class Song
 #
 #   attr_accessor :name
