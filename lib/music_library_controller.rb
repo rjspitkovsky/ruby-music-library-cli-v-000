5ae8d36e5a955e5b10b8f4f81a91e39204c5648b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-19 19:44:33 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,7 +7,7 @@ class MusicLibraryController
 end
 
 def call
-  #input = ""
+  input = ""
   while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
