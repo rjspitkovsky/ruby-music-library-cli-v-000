cd49ee98110b4cf59a5b0c20a1582d8060c0eaaa
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-19 19:19:20 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,15 +6,6 @@ class MusicImporter
     @path = path
   end
 
-    #def files
-    #   Dir.entries(path).each do |filename|
-    #     @@filenames << filename
-    #   end
-    #   @@filenames.reject! do |filename|
-    #     filename == "." || filename == ".."
-    #   end
-    # end
-
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |filename| filename.gsub("#{path}/", "") }
   end
@@ -22,36 +13,6 @@ class MusicImporter
   def import
     files.each do |file|
       Song.create_from_filename(file)
-    end 
+    end
   end
 end
-
-#   #
-#   # attr_accessor :path
-#   #
-#   # @@filenames = []
-#   #
-#   # def initialize(path)
-#   #   @path = path
-#   # end
-#   #
-#   # def files
-#   #   Dir.entries(path).each do |filename|
-#   #     @@filenames << filename
-#   #   end
-#   #   @@filenames.reject! do |filename|
-#   #     filename == "." || filename == ".."
-#   #   end
-#   # end
-#   #
-#   # def self.filenames
-#   #   @@filenames
-#   # end
-#   #
-#   # def import
-#   #   @@filenames.uniq.each do |filename|
-#   #     Song.create_from_filename(filename)
-#   #   end
-#   # end
-#
-# end
