2d11942570c8ea89c7ca135dbe172e28256609da
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-19 19:36:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,27 +60,31 @@ class Song
       end
     end
 
-  # def self.find_by_name(name)
-  #   all.detect{ |s| s.name == name }
-  # end
-  #
-  # def self.find_or_create_by_name(name)
-  #   find_by_name(name) || create(name)
-  # end
-
   def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
-
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-
-    new(song_name, artist, genre)
-  end
-
-  def self.create_from_filename(filename)
-    new_from_filename(filename).tap{ |s| s.save }
-  end
+     array = filename.split(" - ")
+     artist_name = array[0]
+     artist = Artist.find_or_create_by_name(artist_name)
+     title = array[1]
+     genre_name = array[2].chomp(".mp3")
+     genre = Genre.find_or_create_by_name(genre_name)
+     new_song = Song.new(title, artist, genre)
+     end
+   
+     def self.create_from_filename(filename)
+       self.new_from_filename(filename).save
+     end
+  #   parts = filename.split(" - ")
+  #   artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+  # 
+  #   artist = Artist.find_or_create_by_name(artist_name)
+  #   genre = Genre.find_or_create_by_name(genre_name)
+  # 
+  #   new(song_name, artist, genre)
+  # end
+  # 
+  # def self.create_from_filename(filename)
+  #   new_from_filename(filename).tap{ |s| s.save }
+  # end
 end
 
 
